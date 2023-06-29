1e5060285c795899f167813e7effc161d2db0e4a
mike-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2023-06-29 21:52:15 +0100

    Dict integration (lol)

--- maccle_code.exs
+++ maccle_code.exs
@@ -1,8 +1,6 @@
 defmodule MaccleCode.Shared do
-  defmacro is_letter(letter) when is_binary(letter) do
-    quote do
-      String.length(unquote(letter)) == 1 && String.match?(unquote(letter), ~r/[[:alpha:]]/)
-    end
+  def letter?(letter) do
+    String.length(letter) == 1 && String.match?(letter, ~r/[[:alpha:]]/)
   end
 end
 
@@ -10,31 +8,34 @@ defmodule MaccleCode do
   import MaccleCode.Shared
 
   @type message :: String.t()
+  @type message_part :: list(String.t())
+  @type message_parts :: list(message_part())
+
+  # The most common letters in English, apparently
+  @common_letters ~w(e t a o i n s r h l)
 
   @spec start(message()) :: any()
   def start(message) when is_binary(message) do
+    format_message_to_encode(message)
+  end
+
+  @spec start(message()) :: {list(String.t()), message_parts()}
+  defp format_message_to_encode(message) when is_binary(message) do
     message
     |> String.split(" ", trim: true)
     |> Enum.map(fn message_part ->
-      # TODO: This will not handle none alpha characters
       message_part
       |> String.graphemes()
-      |> Enum.filter(&is_letter/1)
+      |> Enum.filter(&letter?/1)
+      |> Enum.map(&String.downcase/1)
+    end)
+    |> then(fn message_parts ->
+      unique_letters =
+        message_parts
+        |> List.flatten()
+        |> Enum.uniq()
 
-      # TODO: Ideally -
-      # Filter unique letters
-      # Make a query to dict for each one
-      # Persist the results
-      # Right now we do it all at once
-
-      |> Enum.map(fn letter ->
-        # So, use the moby thesaurus dictionary as that seems to be the best or at
-        # least easiest to understand and parse
-        # Match (rather than query!) using POSIX regex
-        # I'm unsure whether or not this returns like, every word beginning with the 
-        # provided letter
-        "dict -d 'moby-thesaurus' -m -s re '^#{String.downcase(letter)}.*$'"
-      end)
+      {unique_letters, message_parts}
     end)
   end
 end
@@ -42,9 +43,33 @@ end
 defmodule MaccleCode.Dict do
   import MaccleCode.Shared
 
-  @attributes ["-d", "moby-thesaurus", "-m", "-s", "-re"]
+  @attributes ~w(--database moby-thesaurus --formatted --match --strategy re)
 
-  def words_beginning_with(letter) when is_letter(letter) do
+  def words_beginning_with(letter) do
+    if letter?(letter) do
+      attributes = @attributes ++ ["^#{letter}.*$"]
+
+      try do
+        case System.cmd("dict", attributes) do
+          {result, 0} ->
+            words =
+              result
+              |> String.split("\n", trim: true)
+              |> Enum.map(&String.split(&1, "\t", trim: true))
+              |> Enum.map(&List.last/1)
+              |> Enum.filter(&(!String.contains?(&1, " ")))
+
+            {:ok, words}
+
+          _ ->
+            {:error, letter}
+        end
+      rescue
+        e in ErlangError -> {:error, e}
+      end
+    else
+      {:error, letter}
+    end
   end
 end
 
@@ -53,44 +78,139 @@ ExUnit.start(exclude: [:ignore])
 defmodule MaccleCode.Test do
   use ExUnit.Case, async: true
 
-  @tag :ignore
-  test "it splits a message on spaces" do
-    message = "Hello world, it's nice to be here!"
+  describe "MaccleCode.start/1" do
+    @tag :ignore
+    test "it splits a message on spaces" do
+      message = "Hello world, it's nice to be here!"
 
-    expected = ["Hello", "world,", "it's", "nice", "to", "be", "here!"]
+      expected = ~w(Hello world it's nice to be here!)
 
-    result = MaccleCode.start(message)
+      result = MaccleCode.start(message)
 
-    assert ^result = expected
+      assert ^result = expected
+    end
+
+    @tag :ignore
+    test "it ignores two or more spaces" do
+      message = "Hello  world, it's     nice to be here!"
+
+      expected = ~w(Hello world it's nice to be here!)
+
+      result = MaccleCode.start(message)
+
+      assert ^result = expected
+    end
+
+    @tag :ignore
+    test "it splits a message part into letters" do
+      message = "Hello world, it's nice to be here!"
+
+      expected = [
+        ~w(H e l l o),
+        ~w(w o r l d),
+        ~w(i t s),
+        ~w(n i c e),
+        ~w(t o),
+        ~w(b e),
+        ~w(h e r e)
+      ]
+
+      result = MaccleCode.start(message)
+
+      assert ^result = expected
+    end
+
+    test "it returns a split message and unique letters" do
+      message = "Hello world, it's nice to be here!"
+
+      unique_letters = ~w(h e l o w r d i t s n c b)
+
+      split_message = [
+        ~w(h e l l o),
+        ~w(w o r l d),
+        ~w(i t s),
+        ~w(n i c e),
+        ~w(t o),
+        ~w(b e),
+        ~w(h e r e)
+      ]
+
+      expected = {unique_letters, split_message}
+
+      result = MaccleCode.start(message)
+
+      assert ^result = expected
+    end
   end
 
-  @tag :ignore
-  test "it ignores two or more spaces" do
-    message = "Hello  world, it's     nice to be here!"
+  describe "MaccleCode.Dict.words_beginning_with/1" do
+    @tag :ignore
+    test "it returns an ok tuple" do
+      letter = "h"
 
-    expected = ["Hello", "world,", "it's", "nice", "to", "be", "here!"]
+      expected = {:ok, letter}
 
-    result = MaccleCode.start(message)
+      result = MaccleCode.Dict.words_beginning_with(letter)
 
-    assert ^result = expected
-  end
+      assert ^result = expected
+    end
 
-  @tag :ignore
-  test "it splits a message part into letters" do
-    message = "Hello world, it's nice to be here!"
+    @tag :ignore
+    test "it returns an error tuple if not provided with a letter" do
+      character = "!"
 
-    expected = [
-      ["H", "e", "l", "l", "o"],
-      ["w", "o", "r", "l", "d"],
-      ["i", "t", "s"],
-      ["n", "i", "c", "e"],
-      ["t", "o"],
-      ["b", "e"],
-      ["h", "e", "r", "e"]
-    ]
+      expected = {:error, character}
 
-    result = MaccleCode.start(message)
+      result = MaccleCode.Dict.words_beginning_with(character)
 
-    assert ^result = expected
+      assert ^result = expected
+    end
+
+    @tag :ignore
+    test "it strips the results of garbage" do
+      results = "
+dict.org	2628	moby-thesaurus	ha ha
+dict.org	2628	moby-thesaurus	haberdasher
+dict.org	2628	moby-thesaurus	haberdashery
+dict.org	2628	moby-thesaurus	habit
+dict.org	2628	moby-thesaurus	habitat
+dict.org	2628	moby-thesaurus	habitation
+dict.org	2628	moby-thesaurus	habitual
+dict.org	2628	moby-thesaurus	habitually
+dict.org	2628	moby-thesaurus	habituate
+dict.org	2628	moby-thesaurus	habituation
+dict.org	2628	moby-thesaurus	habitue
+dict.org	2628	moby-thesaurus	hachure
+dict.org	2628	moby-thesaurus	hacienda
+dict.org	2628	moby-thesaurus	hack
+dict.org	2628	moby-thesaurus	hack it"
+
+      expected = ~w(
+        haberdasher
+        haberdashery
+        habit
+        habitat
+        habitation
+        habitual
+        habitually
+        habituate
+        habituation
+        habitue
+        hachure        
+        hacienda      
+        hack
+      )
+
+      result =
+        results
+        |> String.split("\n", trim: true)
+        |> Enum.map(&String.split(&1, "\t", trim: true))
+        |> Enum.map(&List.last/1)
+        |> Enum.filter(&(!String.contains?(&1, " ")))
+
+      IO.inspect(result, label: "result")
+
+      assert ^expected = result
+    end
   end
 end
