a96be7c754aa86ffd772bc07fd547a9236c459ae
mike-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2023-07-09 01:57:05 +0100

    LiveView!

--- lib/maccle_code_web/live/message_live.ex
+++ lib/maccle_code_web/live/message_live.ex
@@ -2,34 +2,20 @@ defmodule MaccleCodeWeb.MessageLive do
   use MaccleCodeWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, form: %{name: "encode_message"}, message_to_encode: "")}
+    {:ok, assign(socket, form: to_form(%{}), message_to_encode: "", encoded_message: "")}
   end
 
   def render(assigns) do
     ~H"""
-      <.simple_form for={@form} phx-change="change">
-        <.input
-          name="message-to-encode"
-          type="textarea"
-          label="Message"
-          value="" />
-      </.simple_form>
+    <.simple_form for={@form}>
+      <.input field={@form[:message_to_encode]} type="textarea" label="Message" phx-change="change" />
+    </.simple_form>
     """
   end
 
-  def handle_event("change", %{"key" => key, "value" => value}, socket) do
-    {:noreply, update(socket, :message_to_encode, fn message_to_encode ->
-
-      IO.inspect(message_to_encode, label: "Message to encode")
-      IO.inspect({key, value}, label: "Keydown")
-
-      result = value <> key
-
-      IO.inspect(result, label: "Result")
-    end)}
+  def handle_event("change", %{"message_to_encode" => message_to_encode}, socket) do
+    {:noreply, update(socket, :message_to_encode, fn _ -> message_to_encode end)}
   end
-  
-  def handle_event("keydown", _, socket) do
-    {:noreply, socket}
-  end
+
+  def handle_event("change", _, socket), do: {:noreply, socket}
 end
