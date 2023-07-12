f7ad8c4e089d0263de1da70f3d51714578af0e78
mike-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2023-07-12 17:35:41 +0100

    fly.io config

--- config/test.exs
+++ config/test.exs
@@ -8,8 +8,7 @@ config :maccle, MaccleWeb.Endpoint,
   server: false
 
 # In test we don't send emails.
-config :maccle, Maccle.Mailer,
-  adapter: Swoosh.Adapters.Test
+config :maccle, Maccle.Mailer, adapter: Swoosh.Adapters.Test
 
 # Disable swoosh api client as it is only required for production adapters.
 config :swoosh, :api_client, false
